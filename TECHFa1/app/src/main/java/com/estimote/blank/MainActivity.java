a307a2f32a506e93cbb59f8354c6d0785ead1bce
Daniel Rovell <danrovell@gmail.com>
2018-03-14 22:26:40 -0500

    Added Google sign in and logout
    Was able to add google sign in and log out. the log in page automatically puts you in hte main menu after logging in. you can return to the login page with the android back button. The Main Menu button on hte login page after signin doesnt work. dont know why yet. need to fix. the logo is a placeholder. Just implemented functionality, can make it pretty later.

--- TECHFa1/app/src/main/java/com/estimote/blank/MainActivity.java
+++ TECHFa1/app/src/main/java/com/estimote/blank/MainActivity.java
@@ -1,6 +1,8 @@
 package com.estimote.blank;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,49 +10,145 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-//
-// Running into any issues? Drop us an email to: contact@estimote.com
-//
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+
+    private SignInButton SignIn;
+    private Button SignOut, MainMenu;
+    private ImageView Logo;
+    private TextView Name;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_Code = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        SignIn = (SignInButton)findViewById(R.id.bn_login);
+        SignOut = (Button)findViewById(R.id.bn_logout);
+        MainMenu = (Button)findViewById(R.id.bn_mainMenu);
+        Logo = (ImageView)findViewById(R.id.Logo);
+        Name = (TextView)findViewById(R.id.name);
+        SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
+        Name.setVisibility(View.GONE);
+        SignOut.setVisibility(View.GONE);
+        MainMenu.setVisibility(View.GONE);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.bn_login:
+                signIn();
+                break;
+            case R.id.bn_logout:
+                signOut();
+                break;
+            case R.id.bn_mainMenu:
+                mainMenu();
+                break;
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+    {
+
+    }
+
+    private void signIn()
+    {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent, REQ_Code);
+    }
+
+    private  void signOut()
+    {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+
+    }
+
+    private void mainMenu()
+    {
+        Intent switchToMainMenu = new Intent(this, MainMenu.class);
+        startActivity(switchToMainMenu);
+        
+    }
+
+    private void handleResult(GoogleSignInResult result)
+    {
+        if(result.isSuccess())
+        {
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            String img_url = account.getPhotoUrl().toString();
+            Name.setText(name);
+            updateUI(true);
+
+            Intent switchToHomepage = new Intent(this, MainMenu.class);
+            startActivity(switchToHomepage);
+
+        }
+        else
+        {
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isLogin)
+    {
+        if(isLogin)
+        {
+            SignIn.setVisibility(View.GONE);
+            Name.setVisibility(View.VISIBLE);
+            MainMenu.setVisibility(View.VISIBLE);
+            SignOut.setVisibility(View.VISIBLE);
+
+        }
+        else
+        {
+            SignIn.setVisibility(View.VISIBLE);
+            Name.setVisibility(View.GONE);
+            MainMenu.setVisibility(View.GONE);
+            SignOut.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==REQ_Code)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
+    }
+
 }
---
+++ TECHFa1/app/src/main/java/com/estimote/blank/MainMenu.java
@@ -0,0 +1,30 @@
+package com.estimote.blank;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainMenu extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+}
